c108fa55cbdc0245661353b6e40b31e148159959
Swapnil Hadge <hadge.swapnil2012@gmail.com>
2026-04-23 00:33:59 +0530

    cleanup after rename

--- rate-limiter-service/REAME.md
+++
@@ -1,6 +0,0 @@
-# Backend Machine Coding Practice
-
-This repository contains backend machine coding exercises implemented in Node.js.
-
-## Problems
-- Rate Limiter
--- shopping-kart-service/discountStrategies/DiscountStragey.js
+++
@@ -1,7 +0,0 @@
-class DiscountStrategy {
-    apply(amount) {
-        throw new Error("apply() must be implemented");
-    }
-}
-
-module.exports = DiscountStrategy
